b22d58e047b77f4ded508e2cbb83b044e3daa197
Sameer Koujalage <159868207+SameerKoujalge@users.noreply.github.com>
2025-11-08 21:52:35 +0530

    Add files via upload

---
+++ Mathematical Operations.py
@@ -0,0 +1,15 @@
+m=int(input("Enter the first number:"))
+n=int(input("Enter the Second number:"))
+
+Addition=m+n
+Substraction=m-n
+Multiplication=m*n
+Division=m/n
+
+print("Addition:", Addition)
+print("Substraction:", Substraction)
+print("Multiplication:", Multiplication)
+print("Division:", Division)
+
+
+
